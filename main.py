5096ae9ac51756fc5b0238880097213ad24a5c5a
Zapdos <71247896+ThatGuyZapdos@users.noreply.github.com>
2021-04-04 15:19:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import jishaku
+import datetime
+
+client = commands.Bot(command_prefix = "e!")
+client.launch_time = datetime.datetime.utcnow() 
+
+@client.command()
+async def ping(ctx):
+    await ctx.message.reply(f"Pong! {round(client.latency * 1000)} ms")
+   
+@client.event
+async def on_ready():
+    c = 0
+    for guild in client.guilds:
+        c += guild.member_count
+    client.load_extension('jishaku')
+    print(f"Bot ready; started on {client.launch_time} and logged in as {client.user} Can see {c} members and {len(client.guilds)} guilds Ping is {round(client.latency * 1000)} ms")
+    
+    
+client.run("TOKEN")
