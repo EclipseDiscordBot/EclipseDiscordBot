aa525ecd4306f5090fecba24bea0a65c0b0a0b94
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-04 16:05:00 +0530

    Using pickle for credentials

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import discord
 from discord.ext import commands
 import jishaku
+import pickle
 import datetime
 
-client = commands.Bot(command_prefix = "e!")
-client.launch_time = datetime.datetime.utcnow() 
+client = commands.Bot(command_prefix="e!")
+client.launch_time = datetime.datetime.utcnow()
+
 
 @client.command()
 async def ping(ctx):
     await ctx.message.reply(f"Pong! {round(client.latency * 1000)} ms")
-   
+
+
 @client.event
 async def on_ready():
     c = 0
     for guild in client.guilds:
         c += guild.member_count
     client.load_extension('jishaku')
-    print(f"Bot ready; started on {client.launch_time} and logged in as {client.user} Can see {c} members and {len(client.guilds)} guilds Ping is {round(client.latency * 1000)} ms")
-    
-    
-client.run("TOKEN")
+    print(
+        f"Bot ready; started on {client.launch_time} and logged in as {client.user} Can see {c} members and {len(client.guilds)} guilds Ping is {round(client.latency * 1000)} ms")
+
+
+token = pickle.load(open("credentials.pkl", 'rb'))['discord']
+client.run(token)
