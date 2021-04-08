cce554c5e6a013aaa52fe8e3e7124460b7db10a8
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-08 20:28:12 +0530

    Added a new exception to handle changed more sends to replies

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -28,60 +28,66 @@ class ErrorHandler(commands.Cog):
         if isinstance(error, commands.DisabledCommand):
             title = "Disabled Command"
             dsc = f"`{ctx.command}` has been disabled by the developers. Please try later!"
-            embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+            await ctx.reply(embed=embed)
 
         elif isinstance(error, commands.NoPrivateMessage):
             title = "Server Command"
             dsc = f"`{ctx.command}` can only be used in a server!"
-            embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+            await ctx.reply(embed=embed)
 
         elif isinstance(error, commands.UserInputError):
             title = "Input Error"
             dsc = "Oops! You've made a mistake while giving me input! Please correct it and try again!"
-            embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+            await ctx.reply(embed=embed)
 
         elif isinstance(error, commands.CommandOnCooldown):
             title = "Command on cooldown"
             dsc = f"`{ctx.command}` is on cooldown! Please try again after {error.retry_after} seconds!"
-            embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+            await ctx.reply(embed=embed)
 
         elif isinstance(error, commands.MemberNotFound):
             title = "Member not found"
             dsc = f"I couldn't find member {error.argument} in this server!"
-            embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-            await ctx.send(embed=embed)
+            embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+            await ctx.reply(embed=embed)
+
+        elif isinstance(error, discord.errors.Forbidden):
+            title = "I don't have the permissions"
+            dsc = f"Are you sure i have administrator permissions?"
+            embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+            await ctx.reply(embed=embed)
 
         elif isinstance(error, commands.MissingPermissions):
             title = "Missing Permissions"
             if len(error.missing_perms) == 1:
                 perm = error.missing_perms[0]
                 dsc = f"You are missing {perm} permission to run `{ctx.command}`"
-                embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-                await ctx.send(embed=embed)
+                embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+                await ctx.reply(embed=embed)
             else:
                 str_perms = ""
                 for perm in error.missing_perms:
                     str_perms += f"{perm}, "
                     dsc = f"You are missing these permissions to run `{ctx.command}`: {str_perms}"
-                    embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-                    await ctx.send(embed=embed)
+                    embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+                    await ctx.reply(embed=embed)
         elif isinstance(error, commands.BotMissingPermissions):
             title = "Missing Permissions"
             if len(error.missing_perms) == 1:
                 perm = error.missing_perms[0]
                 dsc = f"You are missing {perm} permission to run `{ctx.command}`"
-                embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
-                await ctx.send(embed=embed)
+                embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
+                await ctx.reply(embed=embed)
             else:
                 str_perms = ""
                 for perm in error.missing_perms:
                     str_perms += f"{perm}, "
                     dsc = f"You are missing these permissions to run `{ctx.command}`: {str_perms}"
-                    embed = discord.Embed(title=title, description=dsc, color=discord.Color.from_rgb(255, 0, 0))
+                    embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
                     await ctx.reply(embed=embed)
 
         else:
