9937c01d76fe4508c33397ebdd442ffb504fc385
Zapdos <71247896+ThatGuyZapdos@users.noreply.github.com>
2021-04-08 12:12:27 +0530

    Update error_handler.py

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -1,33 +1,81 @@
 import discord
-import asyncio
+import traceback
+import sys
 from discord.ext import commands
 
 
 class ErrorHandler(commands.Cog):
 
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error: Exception):
-        if isinstance(error, discord.ext.commands.CommandNotFound):
+    async def on_command_error(self, ctx, error):
+        if hasattr(ctx.command, 'on_error'):
             return
-        e = discord.Embed(title=f"Sorry! the bot has run into an Error `{error}`",
-                          description="It has been reported to the "
-                                      "devs! *if you're a dev PRs are "
-                                      "open("
-                                      "https://github.com"
-                                      "/EclipseDiscordBot"
-                                      "/EclipseDiscordBot)")
-        await ctx.send(embed=e)
 
-    @commands.is_owner()
-    @commands.command(name="test", hidden=True)
-    async def test(self, ctx, cmd: str):
-        await ctx.send(f"ok! testing {cmd}")
-        if cmd == "errorhandler" or cmd == "error":
-            raise Exception
+        cog = ctx.cog
+        if cog:
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                return
 
+        ignored = (commands.CommandNotFound, commands.NotOwner)
+        error = getattr(error, 'original', error)
 
-def setup(bot: commands.Bot):
+        if isinstance(error, ignored):
+            return
+
+        if isinstance(error, commands.DisabledCommand):
+            title = "Disabled Command"
+            dsc = f"`{ctx.command}` has been disabled by the developers. Please try later!"
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            title = "Server Command"
+            dsc = f"`{ctx.command}` can only be used in a server!"
+
+        elif isinstance(error, commands.UserInputError):
+            title = "Input Error"
+            dsc = "Oops! You've made a mistake while giving me input! Please correct it and try again!"
+        
+        elif isinstance(error, commands.CommandOnCooldown):
+            title = "Command on cooldown"
+            dsc = f"`{ctx.command}` is on cooldown! Please try again after {error.retry_after} seconds!"
+            
+        elif isinstance(error, commands.MemberNotFound):
+            title = "Member not found"
+            dsc = f"I couldn't find member {error.argument} in this server!"
+            
+        elif isinstance(error, commands.MissingPermissions):
+            title = "Missing Permissions"
+            if len(error.missing_perms) == 1:
+                perm = error.missing_perms[0]
+                dsc = f"You are missing {perm} permission to run `{ctx.command}`"
+            else:
+                str_perms = ""
+                for perm in error.missing_perms:
+                    str_perms += f"{perm}, "
+                    dsc = f"You are missing these permissions to run `{ctx.command}`: {str_perms}"
+        elif isinstance(error, commands.BotMissingPermissions):
+            title = "Missing Permissions"
+            if len(error.missing_perms) == 1:
+                perm = error.missing_perms[0]
+                dsc = f"You are missing {perm} permission to run `{ctx.command}`"
+            else:
+                str_perms = ""
+                for perm in error.missing_perms:
+                    str_perms += f"{perm}, "
+                    dsc = f"You are missing these permissions to run `{ctx.command}`: {str_perms}"            
+            
+                
+        
+        
+        
+        embed = discord.Embed(title = title, description = dsc, color = discord.Color.from_rgb(255,0,0))
+        await ctx.send(embed = embed
+        
+        else:
+            print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+def setup(bot):
     bot.add_cog(ErrorHandler(bot))
