cae5633678586985b3afbd6cb593fd86835195b8
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-08 16:48:57 +0530

    Trying something

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -9,7 +9,7 @@ class ErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener
+    @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if hasattr(ctx.command, 'on_error'):
             return
