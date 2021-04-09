f2c571da8a7fbe2331ff201a4aae4aad7b85571e
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-09 21:45:09 +0530

    ...

--- startupfile.sh
+++ startupfile.sh
@@ -1,9 +1,10 @@
 echo "Initializing Bot now..."
-echo "Installing deps"
-pip install -r requirements.txt
 echo "Pulling from GitHub..."
 git reset --hard HEAD
 git pull "https://github.com/EclipseDiscordBot/EclipseDiscordBot.git" --allow-unrelated-histories
 git commit -F notes.txt
 echo "Pulling and Merging successful "
+echo "Installing deps"
+pip install -r requirements.txt
+echo "Done installing deps!"
 python main.py
