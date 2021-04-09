36070c7d6fa43d2cfa9c16409fe7ff12bc89ce32
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-09 11:55:28 +0530

    ...

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -94,8 +94,6 @@ class ErrorHandler(commands.Cog):
                     await ctx.reply(embed=embed)
 
         else:
-            print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
             title = "Unknown exception"
             dsc = f"Sorry, the bot has run into an unknown Exception, it has been reported and is soon to be fixed!"
             embed = discord.Embed(title=title, description=dsc, color=discord.Color.random())
