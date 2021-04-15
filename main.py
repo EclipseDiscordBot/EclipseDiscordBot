8be918a1ff53e0e8752c32a6b319256510d87ea3
Zapdos <71247896+ThatGuyZapdos@users.noreply.github.com>
2021-04-15 11:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,36 +10,33 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="e!", intents=intents)
 slash = SlashCommand(bot, override_type=True)
 bot.launch_time = datetime.datetime.utcnow()
-bot.color = discord.Color.from_rgb(68, 11, 212)
-
-cogs = ['cogs.basic',
-        'cogs.mod',
-        'cogs.error_handler',
-        'cogs.utils',
-        'cogs.fun',
-        'cogs.hypixel',
-        'cogs.slash_cmds.fun',
-        'cogs.slash_cmds.basic',
-        'cogs.slash_cmds.mod',
-        'cogs.giveaways',
-        'jishaku']
+bot.color = discord.Color.from_rgb(156, 7, 241)
 
 
 @bot.event
 async def on_ready():
-    for i in cogs:
-        bot.load_extension(i)
-        print(f"loaded {i}")
-    await send_init_msg()
-    print(
-        f"Bot ready; started on {bot.launch_time} and logged in as {bot.user} Can see {len(bot.users)} members and {len(bot.guilds)} guilds Ping is {round(bot.latency * 1000)} ms")
-
-
-async def send_init_msg():
-    channel = bot.get_channel(827737123704143890)
-    await channel.send(
-        f"Bot ready; started on {bot.launch_time} and logged in as {bot.user} Can see {len(bot.users)} members and {len(bot.guilds)} guilds Ping is {round(bot.latency * 1000)} ms")
-
+        exceptions = ""
+        tracebacks = ""
+        for file in os.listdir("./cogs"):
+                if file.endswith('.py'):
+                        try:
+                                client.load_extension(f"cogs.{file[:-3]}")
+                        except Exception as e:
+                                exceptions += f"- {file} failed to load\n"
+                                tracebacks += f"{traceback.format_exc()}\n"
+                        else:
+                                exceptions += f"+ {file} loaded successfully"
+   
+        embed = discord.Embed(title = "Bot ready!", description = f"Cogs status: \n ```diff\n{exceptions}```", color = bot.color)
+        if tracebacks != "":
+                embed.add_field(name = "Exceptions", value = f"```py\n{tracebacks}")
+        embed.timestamp = bot.launch_time
+        embed.set_footer(text = "Bot online since:")
+        c = bot.get_channel(827737123704143890)
+        await c.send(embed = embed)
+                    
+         
+                               
 
 token = pickle.load(open("credentials.pkl", 'rb'))['discord']
 bot.run(token)
