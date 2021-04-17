c229b9a561c2a9d2aa5da885e7c5df725bd37688
Zapdos <71247896+Zapd0s@users.noreply.github.com>
2021-04-17 19:03:54 +0530

    Create status_roles.py

---
+++ cogs/status_roles.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import commands
+
+class status_roles(commands.Cog):
+  def __init__(self,bot):
+    self.bot = bot
+    
+    
+  @commands.group(invoke_without_command = True)
+  @commands.has_permissions(administrator = True)
+  @commands.guild_only()
+  async def sr(self,ctx):
+    async with self.bot.pool.acquire() as conn:
+      async with conn.transaction():
+        role_id = await conn.fetchval("SELECT role_id FROM sr WHERE guild_id = $1", ctx.guild.id)
+        key = await conn.fetchval("SELECT key FROM sr WHERE guild_id = $1", ctx.guild.id)
+    role = ctx.guild.get_role(role_id)
+    if role is None:
+      txt = "Not setup yet"
+    else:
+      txt = role.mention
+    if key == "placeholder":
+      key = "Not setup yet"
+    embed = discord.Embed(title = "Status Roles", description = f"Status roles gives a role to a person if they have a certain keyword in their custom activity.\n **Here is {ctx.guild.name}'s configuration**", color = self.bot.color)
+    embed.add_field(name = "Role", value = txt)
+    embed.add_field(name = "Keyword", value = key)
+    await ctx.send(embed = embed)
+    
+    
+    @sr.command()
+    @commands.has_permissions(administrator = True)
+    @commands.guild_only()
+    async def keyword(self,ctx,keyword):
+      async with self.bot.pool.acquire() as conn:
+        async with conn.transaction():
+          await conn.execute("UPDATE sr SET keyword = $1 WHERE guild_id = $1", ctx.guild.id)
+          role_id = await conn.fetchval("SELECT role_id FROM sr WHERE guild_id = $1", ctx.guild.id)
+    role = ctx.guild.get_role(role_id)
+    if role is None:
+      txt = "Not setup yet"
+    else:
+      txt = role.mention
+    embed = discord.Embed(title = "Status Roles has been updated!", description = f"Status roles gives a role to a person if they have a certain keyword in their custom activity.\n **Here is {ctx.guild.name}'s configuration**", color = self.bot.color)
+    embed.add_field(name = "Role", value = txt)
+    embed.add_field(name = "Keyword", value = keyword)
+    await ctx.send(embed = embed)
+    
+    
+    @sr.command()
+    async def role(self,ctx,role_id):
+      if isinstance(role_id, int):
+        try:
+          role = ctx.guild.get_role(role_id)
+        except Exception:
+          await ctx.send("That role doesn't exist!")
+          return
+      elif isinstance(role_id, discord.Role):
+        role = role_id
+      
+      async with self.bot.pool.acquire() as conn:
+        async with 
+        
+              
+          
+      
+      
+    
+        
+    
