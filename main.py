f10aae7e68fcbbfb1821e1fd1d0b79fd59271db2
ThatGuyZapdos <narendhararasu@gmail.com>
2021-04-20 18:35:05 +0530

    tweaks to giveaway.py

--- main.py
+++ main.py
@@ -105,6 +105,8 @@ async def gend(gw):
     prize = gw["prize"]
     winners = gw["winners"]
     new_embed = msg.embeds[0].copy()
+    end_time = gw["end_time"]
+    await asyncio.sleep(end_time)
     new_embed.description = f"React with 🎉 to enter!\n**Giveaway Ended**\nWinners: **{winners} winners**\nHosted By: {host.mention}"
     await msg.edit(embed=new_embed)
     reactions = msg.reactions[0]
