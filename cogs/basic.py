97b34ee88c71d01191e438383e6427417d1c58d2
Zapdos <71247896+Zapd0s@users.noreply.github.com>
2021-04-22 11:40:31 +0530

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -44,37 +44,7 @@ class Basic(commands.Cog):
     async def servers(self, ctx):
         await ctx.send(len(self.bot.guilds))
 
-    @commands.command(name="suggest", brief="Suggest a command to the devs")
-    @commands.cooldown(5, 5, discord.ext.commands.BucketType.user)
-    async def suggest(self, ctx: commands.Context):
-        def chek(u1):
-            return u1.author == ctx.author
-        await ctx.reply(
-            "Great! your suggestion is valuable! Now send what the new suggestion will do in brief in **1 SINGLE MESSAGE** \n\n **Pro tip: using shift+enter creates a new line without sending the message**")
-        try:
-            message = await self.bot.wait_for('message', timeout=60.0, check=chek)
-        except asyncio.TimeoutError:
-            await ctx.reply("time's up mate, try again!")
-        else:
-            await ctx.reply("Great! now send some detailed description on the new feature in **1 SINGLE MESSAGE**")
-            try:
-                message2 = await self.bot.wait_for('message', timeout=300.0, check=chek)
-            except asyncio.TimeoutError:
-                await ctx.reply("time's up mate, try again!")
-            else:
-                await ctx.reply("Great! Sending the suggestion to the devs")
-                suggestion_channel = await self.bot.fetch_channel(834442086513508363)
-                suggestion_msg = await suggestion_channel.send(f"""
 
-NEW SUGGESTION FROM {ctx.author}
-
-Brief: ```{message.content}```
-Detailed Description: ```{message2.content}```
-
-""")
-                #await suggestion_msg.add_reaction(':white_check_mark:')
-                #await suggestion_msg.add_reaction(':negative_squared_cross_mark:')
-                await ctx.reply("Done! Thanks for suggesting! keep your DMs open from me, so that I can inform you about your suggestion's status")
 
 
 def setup(bot):
