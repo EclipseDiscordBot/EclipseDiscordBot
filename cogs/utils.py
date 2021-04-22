08a5472d67d1729bbce9fb23117117341a26e1c2
Zapdos <71247896+Zapd0s@users.noreply.github.com>
2021-04-22 11:41:57 +0530

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -26,6 +26,51 @@ class Utility(commands.Cog):
             final = seconds_str[1:]
         await ctx.send(f"Difference between the two message ID's is `{final}` seconds!")
 
+        
+        
+    @commands.command(name="suggest", brief="Suggest a command to the devs")
+    @commands.cooldown(5, 5, discord.ext.commands.BucketType.user)
+    async def suggest(self, ctx: commands.Context, *, suggestion=None):
+        if suggestion is None:
+            def chek(u1):
+                return u1.author == ctx.author
+            await ctx.reply(
+                "Great! your suggestion is valuable! Now send what the new suggestion will do in brief in **1 SINGLE MESSAGE** \n\n **Pro tip: using shift+enter creates a new line without sending the message**")
+            try:
+                message = await self.bot.wait_for('message', timeout=60.0, check=chek)
+            except asyncio.TimeoutError:
+                await ctx.reply("time's up mate, try again!")
+                return
+            else:
+                await ctx.reply("Great! now send some detailed description on the new feature in **1 SINGLE MESSAGE**")
+                try:
+                    message2 = await self.bot.wait_for('message', timeout=300.0, check=chek)
+                except asyncio.TimeoutError:
+                    await ctx.reply("time's up mate, try again!")
+                    return
+                else:
+                    await ctx.reply("Great! Sending the suggestion...")
+                    brief = message.content
+                    detailed = message2.content
+        else:
+            brief = "Not specified"
+            detailed = suggestion
+        suggestion_channel = await self.bot.get_channel(834442086513508363)
+        dsc = f"""
+
+
+**Brief:** ```{message.content}```
+**Detailed Description:** ```{message2.content}```
+
+"""             embed = discord.Embed(title = "New Suggestion", description = dsc, color = self.bot.color)
+        embed.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+        embed.set_footer(text = f"From {ctx.guild if ctx.guild else None}")
+        embed.timestamp = ctx.message.created_at
+        suggestion_msg = await suggestion_channel.send(embed = embed)
+        await suggestion_msg.add_reaction(':white_check_mark:')
+        await suggestion_msg.add_reaction(':negative_squared_cross_mark:')
+        await ctx.reply("Done! you can check the your suggestion's reviews in https://discord.gg/qNwb6zdjJ6")
+                
 
 def setup(bot):
     bot.add_cog(Utility(bot))
