529aaeed2d196f333b6f6dfffc434079d6f5b4bd
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 15:59:24 +0530

    Testing actions

--- scripts/python_scripts/increment_version.py
+++ scripts/python_scripts/increment_version.py
@@ -5,16 +5,8 @@ vss = v.read()
 print(v.read())
 vs = vss.split('.')
 newstring = f'{vs[0]}.{vs[1]}.{int(vs[2]) + 1}'
-v.close()
-vn = open(
-    f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt",
-    'w')
 print(newstring)
-vn.truncate(0)
-vn.write(newstring)
-vn.close()
-v = open(
-    f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt",
-    'r')
-print(v.read())
+v.seek(0)
+v.write(newstring)
+v.truncate()
 v.close()
