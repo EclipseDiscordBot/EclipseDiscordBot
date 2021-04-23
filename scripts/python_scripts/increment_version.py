b97b5ea351d8e9b7b33010803bbb6f4dea03e8ab
satyamedh <satyamedh@users.noreply.github.com>
2021-04-23 10:22:34 +0000

    autopep8 action fixes

--- scripts/python_scripts/increment_version.py
+++ scripts/python_scripts/increment_version.py
@@ -1,14 +1,20 @@
-v = open(f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt", 'r')
+v = open(
+    f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt",
+    'r')
 vss = v.read()
 print(v.read())
 vs = vss.split('.')
 newstring = f'{vs[0]}.{vs[1]}.{int(vs[2]) + 1}'
 v.close()
-vn = open(f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt", 'w')
+vn = open(
+    f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt",
+    'w')
 print(newstring)
 vn.truncate(0)
 vn.write(newstring)
 vn.close()
-v = open(f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt", 'r')
+v = open(
+    f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt",
+    'r')
 print(v.read())
 v.close()
