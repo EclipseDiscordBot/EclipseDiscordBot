cd7fd0cbb1b85880a286064a35bd0dafdb0dd50e
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 14:40:58 +0530

    Bug fix

--- scripts/inc_version.bash
+++ scripts/inc_version.bash
@@ -1,5 +1,5 @@
 sudo apt install tree
 echo "Here starts tree"
-tree /home/runner
+tree $GITHUB_WORKSPACE
 echo "here ends tree"
 python "$GITHUB_WORKSPACE/scripts/python_scripts/increment_version.py"
