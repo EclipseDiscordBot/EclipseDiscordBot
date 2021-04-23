133446ed311501ec2650e00be4441078bfa848e5
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 14:43:17 +0530

    Testing actions

--- scripts/python_scripts/increment_version.py
+++ scripts/python_scripts/increment_version.py
@@ -1,4 +1,3 @@
 import importlib
-file = f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.py"
-version = importlib.import_module(file)
-print(version.version)
+v = open(f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.py", 'r')
+print(v.read())
