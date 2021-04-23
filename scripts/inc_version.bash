968164528ae0c6a2e09c14a0c7dbdde99a5c190b
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 15:50:55 +0530

    Testing actions

--- scripts/inc_version.bash
+++ scripts/inc_version.bash
@@ -3,10 +3,5 @@ then
   exit 0
 else
   python "$GITHUB_WORKSPACE/scripts/python_scripts/increment_version.py"
-  git config user.name github-actions
-  git config user.email github-actions@github.com
-  git add .
-  git commit -m "incremented_version"
-  git push
 fi
 
