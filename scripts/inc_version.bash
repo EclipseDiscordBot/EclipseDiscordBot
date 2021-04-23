31db244f126a5e1046f8adcf8c6a1f2bc451f699
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 14:55:13 +0530

    Testing actions

--- scripts/inc_version.bash
+++ scripts/inc_version.bash
@@ -1,1 +1,6 @@
 python "$GITHUB_WORKSPACE/scripts/python_scripts/increment_version.py"
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add .
+git commit -m "incremented_version"
+git push
