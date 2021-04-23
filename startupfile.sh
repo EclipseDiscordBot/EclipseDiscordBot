ae6b8fc27328dd7973ff3c3c741a67615a01fd41
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 21:28:44 +0530

    this might make the bot stable and reboot less often

--- startupfile.sh
+++ startupfile.sh
@@ -2,6 +2,7 @@ echo "Initializing Bot now..."
 echo "Pulling from GitHub..."
 git reset --hard HEAD
 git pull "https://github.com/EclipseDiscordBot/EclipseDiscordBot.git" --allow-unrelated-histories
+git checkout stable
 git commit -F notes.txt
 echo "Pulling and Merging successful "
 echo "Installing deps"
