6a6764f369f4e1c9a2e53690b131eb0c287bcdb0
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 14:48:39 +0530

    Testing actions

--- scripts/inc_version.bash
+++ scripts/inc_version.bash
@@ -1,5 +1,1 @@
-sudo apt install tree
-echo "Here starts tree"
-tree $GITHUB_WORKSPACE
-echo "here ends tree"
 python "$GITHUB_WORKSPACE/scripts/python_scripts/increment_version.py"
--- scripts/python_scripts/increment_version.py
+++ scripts/python_scripts/increment_version.py
@@ -1,3 +1,10 @@
-import importlib
-v = open(f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.py", 'r')
+v = open(f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt", 'r')
+vss = str(v)
 print(v.read())
+vs = vss.split('.')
+newstring = f'{vs[0]}.{vs[1]}.{int(vs[2]) + 1}'
+v.close()
+vn = open(f"/home/runner/work/EclipseDiscordBot/EclipseDiscordBot/constants/version.txt", 'w')
+vn.truncate(0)
+vn.write(newstring)
+vn.close()
