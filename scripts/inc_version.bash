a974ccbabe45ab4f56a7c623569f432d8c12751b
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-23 15:41:55 +0530

    Testing actions

--- scripts/inc_version.bash
+++ scripts/inc_version.bash
@@ -1,10 +1,12 @@
-#python "$GITHUB_WORKSPACE/scripts/python_scripts/increment_version.py"
-#git config user.name github-actions
-#git config user.email github-actions@github.com
-#git add .
-#git commit -m "incremented_version"
-#git push
-echo "test"
-echo "${author}"
-echo "test2"
-echo "${github.event.pusher.name}"
+if [ $author == 'satyamedh' ] || [ $author == 'zapd0s' ]
+then
+  exit 0
+else
+  python "$GITHUB_WORKSPACE/scripts/python_scripts/increment_version.py"
+  git config user.name github-actions
+  git config user.email github-actions@github.com
+  git add .
+  git commit -m "incremented_version"
+  git push
+fi
+
