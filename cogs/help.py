d0f7bfb783ca8f7dc4744ac5327da5389a7301ef
Zapd0s <Zapd0s@users.noreply.github.com>
2021-04-24 09:39:29 +0000

    autopep8 action fixes

--- cogs/help.py
+++ cogs/help.py
@@ -1,11 +1,13 @@
 import discord
 from discord.ext import commands
 
+
 class EclipseHelpCommand(commands.HelpCommand):
     def get_command_signature(self, command):
         return f"{self.clean_prefix}{command.qualified_name} {command.signature}"
+
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title = "Help", color = self.bot.color)
+        embed = discord.Embed(title="Help", color=self.bot.color)
         prefix = self.clean_prefix
         ctx = self.context
         for cog, commands in mapping.items():
@@ -13,20 +15,13 @@ class EclipseHelpCommand(commands.HelpCommand):
             cmd_str = ""
             for cmd in commands:
                 cmd_str += f"`{cmd.qualified_name}, `"
-            embed.add_field(name = cog_name, value = cmd_str)
+            embed.add_field(name=cog_name, value=cmd_str)
         embed.description = f"Type `{prefix}help <command>` for more information on a command\n" \
                             f"You can also type `{prefix}help <category>` for more info on a category" \
                             f"**Note that this help command shows only the commands that you can use**"
-        embed.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
-        embed.set_footer(text = "Eclipse", icon_url = ctx.bot.avatar_url)
-        await ctx.reply(embed = embed)
-
-
-
-
-
-
-
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Eclipse", icon_url=ctx.bot.avatar_url)
+        await ctx.reply(embed=embed)
 
 
 class help(commands.Cog):
@@ -36,11 +31,9 @@ class help(commands.Cog):
         bot.help_command.cog = self
         self.bot = bot
 
-
-
-
     def cog_unload(self):
         self.bot.help_command = self._original_help_command
+
 
 def setup(bot):
     bot.add_cog(help(bot))
