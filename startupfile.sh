5d148f430e8f8ae13b8ea117a4bd1c6138faf98b
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-26 21:45:36 +0530

    updated code so it can run locally on mah pc

--- startupfile.sh
+++ startupfile.sh
@@ -1,11 +1,2 @@
-echo "Initializing Bot now..."
-echo "Pulling from GitHub..."
-git reset --hard HEAD
 git pull "https://github.com/EclipseDiscordBot/EclipseDiscordBot.git" --allow-unrelated-histories
-git checkout stable
-git commit -F notes.txt
-echo "Pulling and Merging successful "
-echo "Installing deps"
-pip install -r requirements.txt
-echo "Done installing deps!"
 python main.py
