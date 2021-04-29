ec926f624c6e9edb38abbbac91308b89a4a390f5
ThatGuyZapdos <narendhararasu@gmail.com>
2021-04-29 16:47:54 +0530

    updated help; fixed greet bugs

--- cogs/help.py
+++ cogs/help.py
@@ -10,13 +10,27 @@ class EclipseHelpCommand(commands.HelpCommand):
 
     async def send_bot_help(self, mapping):
         ctx = self.context
-        embed = discord.Embed(title="Help", description = "Here is a list of my commands!", color = ctx.bot.color)
+        bot_prefix = self.clean_prefix
+        embed = discord.Embed(title="Help", description = f"Type `{bot_prefix}help <command>` for more info on a command.\nYou can also type `{bot_prefix}help <category>` for more info on a category.", color = ctx.bot.color)
         for cog, commands in mapping.items():
             all_cmds = [f"{c.qualified_name}" for c in commands]
             if all_cmds:
                 cog_name = getattr(cog, "qualified_name", "No Category")
                 embed.add_field(name=cog_name, value=" ".join(all_cmds), inline=False)
         await ctx.send(embed = embed)
+
+    async def send_cog_help(self, cog):
+        ctx = self.context
+        embed = discord.Embed(title = f"{cog.qualified_name} Commands", color = ctx.bot.color)
+        all_commands = cog.get_commands()
+        dsc = ""
+        for command in all_commands:
+            dsc += f"**{command.qualified_name}** - {command.breif}\n"
+
+        embed.description=dsc
+        await ctx.send(embed=embed)
+
+
 
 
 
