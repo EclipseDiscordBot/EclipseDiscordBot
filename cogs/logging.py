f92fa46ef89d9d1115e12d76315da74f5609e201
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-04-29 18:02:38 +0530

    removed useless redifened listener

--- cogs/logging.py
+++ cogs/logging.py
@@ -66,30 +66,6 @@ class Logging(commands.Cog):
                     f"Message Edited by {new_edited_msg_content.author}",
                     datetime.datetime.now().timestamp(), 1, 0, 0)
 
-    @commands.Cog.listener("on_raw_message_edit")
-    async def msg_edit(self, msg: discord.RawMessageUpdateEvent):
-        channel: discord.TextChannel = self.bot.get_guild(
-            msg.guild_id).get_channel(msg.channel_id)
-        new_edited_msg_content = await channel.fetch_message(msg.message_id)
-        e = Embed(
-            title="Message Edited",
-            description=f"Message Edited in {channel.mention}")
-        e.add_field(name="New message: ", value=new_edited_msg_content.content)
-        async with self.bot.pool.acquire() as conn:
-            async with conn.transaction():
-                log_channel_ids = await conn.fetch("SELECT * FROM logging WHERE server_id=$1", msg.guild_id)
-                log_channel_id = log_channel_ids[0]['channel_id']
-                log_chnl = self.bot.get_guild(
-                    msg.guild_id).get_channel(log_channel_id)
-                if log_chnl is None:
-                    return
-                await log_chnl.send(embed=e)
-                await conn.execute(
-                    "INSERT INTO logs(server_id,channel_id,msg_id,reason,timestamp,type,mod_id,punished_id)"
-                    "VALUES($1,$2,$3,$4,$5,$6,$7,$8)", msg.guild_id, msg.channel_id, msg.message_id,
-                    f"Message Edited by {new_edited_msg_content.author}",
-                    datetime.datetime.now().timestamp(), 1, 0, 0)
-
 
 def setup(bot: commands.Bot):
     bot.add_cog(Logging(bot))
