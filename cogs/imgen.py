7239740dbed2e9e82a98ffb138ff5bf03d64b93e
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-05-03 19:38:59 +0530

    bug fix

--- cogs/imgen.py
+++ cogs/imgen.py
@@ -4,6 +4,7 @@ import os
 import aiofiles
 import aiohttp
 import discord
+import asyncio
 from discord.ext import commands
 
 
@@ -28,20 +29,23 @@ class ImageGeneration(commands.Cog):
     async def au(self, ctx, user: discord.User = None, *, impostor: bool):
         async with ctx.channel.typing():
             uname = urllib.parse.quote((str(ctx.author) if not user else str(user)))
-            url = (ctx.author.avatar_url if not user else user.avatar_url)
+            av = (ctx.author.avatar_url_as(format='png') if not user else user.avatar_url_as(format='png'))
             impostor = ('true' if impostor else 'false')
             rand = random.randint(0, 10000)
             async with aiohttp.ClientSession() as session:
-                url = f"https://some-random-api.ml/premium/amongus?avatar={url}&username={uname}&impostor={impostor}&key={self.bot.sra_api}"
+                url = f"https://some-random-api.ml/premium/amongus?avatar={av}&username={uname}&impostor={impostor}&key={self.bot.sra_api}"
                 async with session.get(url) as resp:
                     if resp.status == 200:
                         f = await aiofiles.open(f'data/amongus.{rand}.gif', mode='wb')
                         await f.write(await resp.read())
                         await f.close()
+                    else:
+                        await ctx.reply("There was an error with the api")
+                        return
 
             file = discord.File(open(f'data/amongus.{rand}.gif', 'rb'))
             await ctx.reply(file=file)
-            os.system(f"rm data/amongus.{rand}.gif")
+        # os.system(f"rm data/amongus.{rand}.gif")
 
 
 def setup(bot: commands.Bot):
