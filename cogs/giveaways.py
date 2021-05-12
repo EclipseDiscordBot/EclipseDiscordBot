371ade126a6746311efc4e234cc07e2875292e55
ThatGuyZapdos <narendhararasu@gmail.com>
2021-05-12 20:36:28 +0530

    giveaways changes and added rock paper scissors cmd

--- cogs/giveaways.py
+++ cogs/giveaways.py
@@ -78,13 +78,15 @@ class Giveaways(commands.Cog):
 
     @commands.command()
     async def gend(self, ctx, id:int = None):
+        msg_id = 0
         if id is None:
             all_gws = await self.bot.pool.fetch("SELECT msg_id FROM giveaways WHERE ch_id= $1", ctx.channel.id)
             async for message in ctx.channel.history(limit=50):
                 if message.id in all_gws:
-                    id = message.id
+                    msg_id = message.id
                     break
-            if id is None:
+
+            if msg_id is None:
                 await ctx.send("I couldn't find any recent giveaways!")
                 return
         await self.force_end(id)
