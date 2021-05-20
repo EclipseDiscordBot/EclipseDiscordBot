8e1d2712a6d377b6d7f1a33939094ead197cd1bd
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-05-20 12:23:13 +0530

    bug squashed

--- scripts/python_scripts/stats_webhook.py
+++ scripts/python_scripts/stats_webhook.py
@@ -3,9 +3,10 @@ import pickle
 import psutil
 import os
 import humanize
+import datetime
 
 
-def update_stats(bot:discord.ext.commands.Bot):
+async def update_stats(bot:discord.ext.commands.Bot):
     latency = f"{bot.latency * 1000}ms"
 
     msg_id = 844559876684513280
@@ -15,18 +16,19 @@ def update_stats(bot:discord.ext.commands.Bot):
 
     cpu_usage = f"{psutil.cpu_percent(5)}%"
     ram_usage = f"{psutil.virtual_memory()[2]}%"
-    pid = os.getpid()
     load1, load2, load3 = psutil.getloadavg()
     load_str = f"{load1}, {load2}, {load3}"
     embed = discord.Embed(title = "Bot Stats", description = f"Online for **{humanize.precisedelta(datetime.datetime.now() - bot.launch_time)}**", color=bot.color)
     embed.add_field(name="Ping", value=latency)
+    embed.add_field(name="Servers", value=len(bot.guilds))
+    users = 0
+    for guild in bot.guilds:
+        for memb in guild.members:
+            users += 1
+    embed.add_field(name="Users", value=str(users))
     embed.add_field(name="CPU", value=cpu_usage)
     embed.add_field(name="RAM", value=ram_usage)
     embed.add_field(name="Load Average", value=load_str)
-    embed.add_field(name="PID", value=str(pid))
     embed.timestamp = datetime.datetime.now()
     embed.set_footer(text="Last updated")
-    await webhook.edit_message(msg_id, content=None, embed=embed)
-
-
-
+    webhook.edit_message(msg_id, content=None, embed=embed)
