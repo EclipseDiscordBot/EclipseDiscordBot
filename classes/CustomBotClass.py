0ab4e53f0adf25ab60a5d3a4a4c7b1408f6f7b55
ThatGuyZapdos <narendhararasu@gmail.com>
2021-05-21 18:09:00 +0530

    blacklist

--- classes/CustomBotClass.py
+++ classes/CustomBotClass.py
@@ -90,5 +90,13 @@ class CustomBot(commands.Bot):
         await self.process_commands(message)
 
     async def process_commands(self, message):
+        blacklists = await self.pool.fetch("SELECT * FROM blacklists")
+        list = []
+        for row in blacklists:
+            list.append(row["id"])
+        if message.author.id in list:
+            return
+        if message.guild.id in list:
+            return
         ctx = await self.get_context(message, cls=context.Context)
         await self.invoke(ctx)
