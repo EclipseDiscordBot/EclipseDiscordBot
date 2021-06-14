dabfbb2cd9b87118c4eae4fb468b1861d92f05a2
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-06-14 10:54:10 +0530

    Revert "simplified economy"
    This reverts commit 8cfed8253815c1875b8ef5e9c6d75ad94bf9aa2f.

--- cogs/economy/basic_commands.py
+++ cogs/economy/basic_commands.py
@@ -2,8 +2,6 @@ import discord
 from discord.ext import commands
 from classes import CustomBotClass, indev_check
 import random
-
-
 class EconomyBasic(commands.Cog):
     def __init__(self, bot: CustomBotClass.CustomBot):
         """
@@ -20,29 +18,17 @@ class EconomyBasic(commands.Cog):
         """
         self.bot = bot
 
-    async def update_account(self, member: discord.Member, column, new_value):
-        if column == "purse":
-            await self.bot.pool.execute("UPDATE economy SET purse=$1 WHERE uid=$2", new_value, member.id)
-        elif column == "bank":
-            await self.bot.pool.execute("UPDATE economy SET bank=$1 WHERE uid=$2", new_value, member.id)
-
-
-
-    async def get_status(self, member: discord.Member):
-        return await self.bot.pool.fetch("SELECT * FROM economy WHERE uid=$1", member.id)
-
     @commands.command("beg", brief="Begs for money!")
     @commands.cooldown(1, 45, commands.BucketType.user)
     @indev_check.command_in_development()
     async def _beg(self, ctx):
-        balance = await self.get_status(ctx.author)
+        current_status = await self.bot.pool.fetch("SELECT * FROM economy WHERE uid=$1", ctx.author.id)
         beg_successful = random.choice([True, False])
         coins = (random.randint(0, 1000) if beg_successful else 0)
-        new_bal = balance[0]["purse"] + coins
-        await self.update_account(ctx.author, "purse", new_bal)
-        e = discord.Embed(title=("Oh poor little beggar! take some coins!" if beg_successful else "Ew! a beggar!"),
-                          description=f"You got {str(coins)} coins!",
-                          colour=(discord.Colour.green() if beg_successful else discord.Colour.red()))
+        e = discord.Embed(title=("Oh poor little beggar! take some coins!" if beg_successful else "Ew! a beggar!"), description=f"You got {str(coins)} coins!", colour=(discord.Colour.green() if beg_successful else discord.Colour.red()))
+        async with self.bot.pool.acquire() as conn:
+            async with conn.transaction():
+                await conn.execute("UPDATE economy SET purse=$1 WHERE uid=$2", current_status[0]['purse']+coins, ctx.author.id)
 
         await ctx.reply(embed=e)
 
