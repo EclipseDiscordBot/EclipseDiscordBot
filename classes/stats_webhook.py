2160aeabd79e0a5e8864b07dc46f2ccd92feea91
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-06-15 21:21:10 +0530

    as usual, fixing zapd0s's shitty code

--- classes/stats_webhook.py
+++ classes/stats_webhook.py
@@ -6,7 +6,8 @@ import humanize
 import datetime
 from discord.ext import commands
 
-async def update_stats(bot:commands.Bot):
+
+async def update_stats(bot: commands.Bot):
     latency = f"{bot.latency * 1000}ms"
 
     msg_id = 844559876684513280
@@ -17,7 +18,9 @@ async def update_stats(bot:commands.Bot):
     ram_usage = f"{psutil.virtual_memory()[2]}%"
     load1, load2, load3 = psutil.getloadavg()
     load_str = f"{load1}, {load2}, {load3}"
-    embed = discord.Embed(title = "Bot Stats", description = f"Online for **{humanize.precisedelta(datetime.datetime.now() - bot.launch_time)}**", color=bot.color)
+    embed = discord.Embed(title="Bot Stats",
+                          description=f"Online for **{humanize.precisedelta(datetime.datetime.utcnow() - bot.launch_time)}**",
+                          color=bot.color)
     embed.add_field(name="Ping", value=latency)
     embed.add_field(name="Servers", value=len(bot.guilds))
     users = 0
@@ -28,7 +31,7 @@ async def update_stats(bot:commands.Bot):
     embed.add_field(name="CPU", value=cpu_usage)
     embed.add_field(name="RAM", value=ram_usage)
     embed.add_field(name="Load Average", value=load_str)
-    embed.timestamp = datetime.datetime.now()
+    embed.timestamp = datetime.datetime.utcnow()
     embed.set_footer(text="Last updated")
     await webhook.edit_message(msg_id, content=None, embed=embed)
     await session.close()
