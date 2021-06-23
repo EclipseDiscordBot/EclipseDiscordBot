474f7f1ea30af9cbe6d021ddd738087eced97e1f
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-06-23 10:19:52 +0530

    fixed bug

--- classes/check_create_db_entries.py
+++ classes/check_create_db_entries.py
@@ -7,5 +7,5 @@ async def check_create_db(bot, user):
     if not row:
         async with bot.pool.acquire() as conn:
             async with conn.transaction():
-                await conn.execute("INSERT INTO economy(uid,purse,bank,inventory,work,sus,passive) VALUES($1,$2,$3,$4,$5,$6,$7)", user.id, 0, 0, str({"legnth": 0}), 0, False, False)
-        await user.send(basic_eco.first_interaction.format(user_mention=user.mention, prefix=ctx.prefix, bot_invite=basic.invite, support_server=basic.support_server))
+                await conn.execute("INSERT INTO economy(uid,purse,bank,work,sus,passive,inventory,active_items,claimed_daily) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)", user.id, 0, 0, 0, False, False, [], [], False)
+        await user.send(basic_eco.first_interaction.format(user_mention=user.mention, prefix="e!", bot_invite=basic.invite, support_server=basic.support_server))
