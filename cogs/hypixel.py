72b7e37fbfc849fba89950360b9273bba169fe76
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2021-06-29 00:17:41 +0530

    It's 12:16am xD anyway, added special paginator, so it uses embed title as the page, also added stats command, skyblock stuff to be done soon as for some weird reason I am getting sleep :/

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -1,10 +1,13 @@
+import json
 import sys
 import aiohttp
 import asyncpixel.exceptions.exceptions
 import discord
 from discord import Embed
 import pickle
-from constants import bz_ids
+
+from classes.paginator import SpecialPaginator
+from constants import bz_ids, emojis
 from classes import CustomBotClass
 from discord.ext import commands, tasks
 
@@ -21,6 +24,15 @@ class Hypixel(commands.Cog):
         # self.auction_loop.start()
         self.bz_id_item = bz_ids.id_name
         self.bz_item_id = bz_ids.name_id
+
+    async def get_uuid(self, username):
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(f"https://api.mojang.com/users/profiles/minecraft/{username}") as res:
+                if not res.status == 200:
+                    return False
+                json = await res.json()
+                return json['id']
+
 
     @commands.command(name="key", aliases=["hypixel_key", "hypixel_api_key"],
                       brief="Gives information about a hypixel api key")
@@ -134,6 +146,60 @@ class Hypixel(commands.Cog):
         print("proccessed hypixel AH")
         # self.currentAh = pickle.load(open('data/test.pkl', 'rb')) # a sample AH
         # self.AHDict = await calc_auc(self.hypixel, self.currentAh, self.bot)
+
+    @commands.command("stats", brief="Gets the hypixel stats of a person")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def _stats(self, ctx: commands.Context, uname: str=None):
+        if uname is None:
+            await ctx.reply("ey! You didn't provide a username! I'm not gonna accept empty usernames :unamused:")
+            return
+        ch: discord.TextChannel = ctx.channel
+        async with ch.typing():
+            uuid = await self.get_uuid(uname)
+            if not uuid:
+                await ctx.reply("Woah! that's a invalid username!")
+                return
+            all_stats = json.loads((await self.hypixel.player(uuid)).json())['raw']['player']
+            random_color = discord.Color.random()
+            # Bedwars
+            bedwars_e = discord.Embed(title=f'Bedwars', colour=random_color)
+            bedwars_e.add_field(name="Stars", value=all_stats['achievements']['bedwars_level'])
+            bedwars_e.add_field(name="Wins", value=all_stats['achievements']['bedwars_wins'])
+            bedwars_e.add_field(name="Total games played", value=all_stats['stats']['Bedwars']['games_played_bedwars'])
+            bedwars_e.add_field(name="Win Streak", value=all_stats['stats']['Bedwars']['winstreak'])
+            bedwars_e.add_field(name="Beds Broken", value=all_stats['achievements']['bedwars_beds'])
+            bedwars_e.add_field(name="Deaths", value=all_stats['stats']['Bedwars']['deaths_bedwars'])
+            bedwars_e.add_field(name="Coins", value=all_stats['stats']['Bedwars']['coins'])
+            bedwars_e.add_field(name="Kills", value=all_stats['stats']['Bedwars']['kills_bedwars'])
+            bedwars_e.add_field(name="Total Items purchased", value=all_stats['stats']['Bedwars']['items_purchased_bedwars'])
+            # Skywars
+            skywars_e = discord.Embed(title=f'Skywars', colour=random_color)
+            skywars_e.add_field(name="Souls", value=all_stats['stats']['SkyWars']['souls'])
+            skywars_e.add_field(name="Soul Well Level", value=all_stats['stats']['SkyWars']['soul_well'])
+            skywars_e.add_field(name="Total games played", value=all_stats['stats']['SkyWars']['games_played_skywars'])
+            skywars_e.add_field(name="Coins", value=all_stats['stats']['SkyWars']['coins'])
+            skywars_e.add_field(name="Deaths", value=all_stats['stats']['SkyWars']['deaths'])
+            skywars_e.add_field(name="Win Streak", value=all_stats['stats']['SkyWars']['win_streak'])
+            skywars_e.add_field(name="Kills", value=all_stats['stats']['SkyWars']['kills'])
+            skywars_e.add_field(name="Kill Streak", value=all_stats['stats']['SkyWars']['killstreak'])
+            skywars_e.add_field(name="Wins", value=all_stats['stats']['SkyWars']['wins'])
+            # Duels
+            duels_e = discord.Embed(title=f'Duels', colour=random_color)
+            duels_e.add_field(name="Coins", value=all_stats['stats']['Duels']['coins'])
+            duels_e.add_field(name="Total Games Played", value=all_stats['stats']['Duels']['games_played_duels'])
+            duels_e.add_field(name="Blocks placed", value=all_stats['stats']['Duels']['blocks_placed'])
+            duels_e.add_field(name="Total damage dealt", value=all_stats['stats']['Duels']['damage_dealt'])
+            duels_e.add_field(name="Wins", value=all_stats['stats']['Duels']['wins'])
+            duels_e.add_field(name="Deaths", value=all_stats['stats']['Duels']['deaths'])
+            duels_e.add_field(name="Total Bridge Goals", value=all_stats['stats']['Duels']['goals'])
+            duels_e.add_field(name="Ping Preference", value=all_stats['stats']['Duels']['pingPreference'])
+            # Skyblock
+            skyblock_e = discord.Embed(title=f'Skyblock', colour=random_color)
+            skyblock_e.add_field(name="Profile name(First profile)", value=list(all_stats['stats']['SkyBlock']['profiles'].values())[0]['cute_name'])
+            prof_id =   list(all_stats['stats']['SkyBlock']['profiles'].values())[0]['profile_id']
+            message = await ctx.reply(f"Fetching data {emojis.loading}")
+            _paginator = SpecialPaginator(message, [bedwars_e, skywars_e, duels_e, skyblock_e])
+            await message.edit(view=_paginator, embed=bedwars_e, content=f"Here are `{uname}`'s stats")
 
 
 def setup(bot):
